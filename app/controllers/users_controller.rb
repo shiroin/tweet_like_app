cdd7ef355b3d63983e0f5f3d5cba65e4da9e98ca
shiroin <shirota.ippei.dd@gmail.com>
2022-04-25 01:03:28 +0900

    Implemeent basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
+
   def show
-      @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
-      @user = User.new
+    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -17,8 +19,10 @@ class UsersController < ApplicationController
     end
   end
 
-    private
-        def user_params
-            params.require(:user).permit(:name, :email, :password, :password_confirmation)
-        end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
